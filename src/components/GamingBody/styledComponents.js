185af1b315eda2d19b7d44db0352fb7cc883052c
tarunmallula <148045910+tarunmallula@users.noreply.github.com>
2023-12-15 13:18:51 +0530

    Update styledComponents.js

--- src/components/GamingBody/styledComponents.js
+++ src/components/GamingBody/styledComponents.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const VideoCardContainer = styled.div`
+export const VideoCardContainer = styled.li`
   display: flex;
   flex-direction: column;
   width: 50%;
